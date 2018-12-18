25d6968d6ef50977708db75f144dd206671fa3b4
Travis Jeffery <tj@travisjeffery.com>
2018-12-18 16:58:58 -0500

    add reset and calls apis

--- cmd/mocker/main.go
+++ cmd/mocker/main.go
@@ -8,16 +8,18 @@ import (
 )
 
 var (
-	src   = kingpin.Arg("src", "File to find interfaces.").String()
-	dst   = kingpin.Flag("dst", "File write mocks. Leave blank to write to Stdout.").String()
-	pkg   = kingpin.Flag("pkg", "Name of package for mocks. Inferred by default.").String()
-	intfs = kingpin.Arg("ifaces", "Interfaces to mock.").Strings()
+	src    = kingpin.Arg("src", "File to find interfaces.").String()
+	dst    = kingpin.Flag("dst", "File write mocks. Leave blank to write to Stdout.").String()
+	pkg    = kingpin.Flag("pkg", "Name of package for mocks. Inferred by default.").String()
+	prefix = kingpin.Flag("prefix", "Prefix of mock names.").Default("Mock").String()
+	suffix = kingpin.Flag("suffix", "Suffix of mock names.").String()
+	intfs  = kingpin.Arg("ifaces", "Interfaces to mock.").Strings()
 )
 
 func main() {
 	kingpin.Parse()
 
-	m, err := mocker.New(*src, *dst, *pkg, *intfs)
+	m, err := mocker.New(*src, *dst, *pkg, *prefix, *suffix, *intfs)
 	if err != nil {
 		log.Fatal("mocker: failed to instantiate")
 	}
