fd17a7d1fed85064765c24a85f22e4f29a2da955
Travis Jeffery <tj@travisjeffery.com>
2018-12-18 07:12:43 -0500

    update to handle modules, etc.

--- cmd/mocker/main.go
+++ cmd/mocker/main.go
@@ -1,46 +1,28 @@
 package main
 
 import (
-	"bytes"
-	"io"
-	"io/ioutil"
 	"log"
-	"os"
 
 	"github.com/travisjeffery/mocker/pkg/mocker"
 	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	out    = kingpin.Flag("out", "File to write mocks to. Stdout by default.").String()
-	pkg    = kingpin.Flag("pkg", "Name of package for mocks. Inferred by default.").String()
-	src    = kingpin.Arg("src", "Directory to find interfaces.").Required().String()
-	iface  = kingpin.Arg("ifaces", "Interfaces to mock.").Required().Strings()
-	prefix = kingpin.Flag("prefix", "Prefix for mocks.").Default("Mock").String()
-	suffix = kingpin.Flag("suffix", "Suffix for mocks.").String()
+	src   = kingpin.Arg("src", "File to find interfaces.").String()
+	dst   = kingpin.Flag("dst", "File write mocks. Leave blank to write to Stdout.").String()
+	pkg   = kingpin.Flag("pkg", "Name of package for mocks. Inferred by default.").String()
+	intfs = kingpin.Arg("ifaces", "Interfaces to mock.").Strings()
 )
 
 func main() {
 	kingpin.Parse()
 
-	var buf bytes.Buffer
-	var w io.Writer
-
-	w = os.Stdout
-	if out != nil {
-		w = &buf
-	}
-
-	m, err := mocker.New(src, pkg, iface, prefix, suffix, w)
+	m, err := mocker.New(*src, *dst, *pkg, *intfs)
 	if err != nil {
 		log.Fatal("mocker: failed to instantiate")
 	}
 
 	if err = m.Mock(); err != nil {
 		log.Fatalf("mocker: failed to mock: %v", err)
-	}
-
-	if out != nil {
-		ioutil.WriteFile(*out, buf.Bytes(), 0777)
 	}
 }
