2d377bf5a8535ec83b2287622362e4f8d13c9c28
Travis Jeffery <tj@travisjeffery.com>
2018-12-20 04:22:47 -0500

    add selfpkg and filtering out interfaces to generate

--- cmd/mocker/main.go
+++ cmd/mocker/main.go
@@ -8,18 +8,19 @@ import (
 )
 
 var (
-	src    = kingpin.Arg("src", "File to find interfaces.").String()
-	dst    = kingpin.Flag("dst", "File write mocks. Leave blank to write to Stdout.").String()
-	pkg    = kingpin.Flag("pkg", "Name of package for mocks. Inferred by default.").String()
-	prefix = kingpin.Flag("prefix", "Prefix of mock names.").Default("Mock").String()
-	suffix = kingpin.Flag("suffix", "Suffix of mock names.").String()
-	intfs  = kingpin.Arg("ifaces", "Interfaces to mock.").Strings()
+	src     = kingpin.Arg("src", "File to find interfaces.").String()
+	dst     = kingpin.Flag("dst", "File write mocks. Leave blank to write to Stdout.").String()
+	pkg     = kingpin.Flag("pkg", "Name of package for mocks. Inferred by default.").String()
+	prefix  = kingpin.Flag("prefix", "Prefix of mock names.").Default("Mock").String()
+	suffix  = kingpin.Flag("suffix", "Suffix of mock names.").String()
+	selfpkg = kingpin.Flag("selfpkg", "The full package import path for the generated code. The purpose of this flag is to prevent import cycles in the generated code by trying to include its own package. This can happen if the mock's package is set to one of its inputs (usually the main one) and the output is stdio so mockgen cannot detect the final output package. Setting this flag will then tell mockgen which import to exclude.").String()
+	intfs   = kingpin.Arg("ifaces", "Interfaces to mock.").Strings()
 )
 
 func main() {
 	kingpin.Parse()
 
-	m, err := mocker.New(*src, *dst, *pkg, *prefix, *suffix, *intfs)
+	m, err := mocker.New(*src, *dst, *pkg, *prefix, *suffix, *selfpkg, *intfs)
 	if err != nil {
 		log.Fatal("mocker: failed to instantiate")
 	}
